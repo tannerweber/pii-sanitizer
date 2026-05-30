4e242e285430fe93c85273feaa3378ec19de3bad
xavier-iz <izard@pdx.edu>
2026-05-30 13:13:03 -0700

    Added text similarity comparison to testing

--- test.py
+++ test.py
@@ -1,7 +1,25 @@
-import os
+import subprocess
+import spacy
 
-inputs_file = open('test_inputs.txt')
-inputs = inputs_file.readlines()
+nlp = spacy.load("en_core_web_md")
 
-for i in inputs:
-    os.system('python3 main.py ' + i)
+with open("test_inputs.txt", "r") as input_files:
+    inputs = input_files.readlines()
+
+for original_text in inputs:
+    original_text = original_text.strip()
+
+    result = subprocess.run(
+        ["python", "main.py", original_text],
+        capture_output=True,
+        text=True
+    )
+
+    anonymized_text = result.stdout.strip()
+
+    orginal_doc = nlp(original_text)
+    anonymized_doc = nlp(anonymized_text)
+
+    print(f"Original: {original_text}")
+    print(f"Anonymized: {anonymized_text}")
+    print(f"Similarity Score: {orginal_doc.similarity(anonymized_doc)} \n")
