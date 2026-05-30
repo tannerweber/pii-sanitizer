68ec24cc790844245b16fcd3fefb2c9940394aa1
benmo-pdx <benmo@pdx.edu>
2026-05-30 13:40:58 -0700

    Update main.py

--- main.py
+++ main.py
@@ -51,68 +51,69 @@ ip_index       = 1
 operators = {}
 for result in recognizer_results:
 
-    if result.entity_type == "PERSON":
-        name = prompt_text[result.start:result.end]
-        result.entity_type = name
-        if name not in name_IDs:
-            name_IDs[name]  = name_index
-            name_index     += 1
-            operators[name] = OperatorConfig(operator_name="replace",
-                                             params={"new_value": f"Person {name_IDs[name]}"})
+    match result.entity_type:
+        case "PERSON":
+            name = prompt_text[result.start:result.end]
+            result.entity_type = name
+            if name not in name_IDs:
+                name_IDs[name]  = name_index
+                name_index     += 1
+                operators[name] = OperatorConfig(operator_name="replace",
+                                                 params={"new_value": f"Person {name_IDs[name]}"})
 
-    elif result.entity_type == "DATE_TIME":
-        date = prompt_text[result.start:result.end]
-        result.entity_type = date
-        if date not in date_IDs:
-            date_IDs[date]  = date_index
-            date_index      += 1
-            operators[date] = OperatorConfig(operator_name="replace",
-                                             params={"new_value": f"Time {date_IDs[date]}"})
+        case "DATE_TIME":
+            date = prompt_text[result.start:result.end]
+            result.entity_type = date
+            if date not in date_IDs:
+                date_IDs[date]  = date_index
+                date_index     += 1
+                operators[date] = OperatorConfig(operator_name="replace",
+                                                 params={"new_value": f"Time {date_IDs[date]}"})
 
-    elif result.entity_type == "US_SSN":
-        ssn = prompt_text[result.start:result.end]
-        result.entity_type = ssn
-        if ssn not in ssn_IDs:
-            ssn_IDs[ssn]   = ssn_index
-            ssn_index     += 1
-            operators[ssn] = OperatorConfig(operator_name="replace",
-                                            params={"new_value": f"SSN {ssn_IDs[ssn]}"})
+        case "US_SSN":
+            ssn = prompt_text[result.start:result.end]
+            result.entity_type = ssn
+            if ssn not in ssn_IDs:
+                ssn_IDs[ssn]   = ssn_index
+                ssn_index     += 1
+                operators[ssn] = OperatorConfig(operator_name="replace",
+                                                params={"new_value": f"SSN {ssn_IDs[ssn]}"})
 
-    elif result.entity_type == "PHONE_NUMBER":
-        phone = prompt_text[result.start:result.end]
-        result.entity_type = phone
-        if phone not in phone_IDs:
-            phone_IDs[phone] = phone_index
-            phone_index     += 1
-            operators[phone] = OperatorConfig(operator_name="replace",
-                                              params={"new_value": f"Phone Number {phone_IDs[phone]}"})
+        case "PHONE_NUMBER":
+            phone = prompt_text[result.start:result.end]
+            result.entity_type = phone
+            if phone not in phone_IDs:
+                phone_IDs[phone] = phone_index
+                phone_index     += 1
+                operators[phone] = OperatorConfig(operator_name="replace",
+                                                  params={"new_value": f"Phone Number {phone_IDs[phone]}"})
 
-    elif result.entity_type == "LOCATION":
-        location = prompt_text[result.start:result.end]
-        result.entity_type = location
-        if location not in location_IDs:
-            location_IDs[location] = location_index
-            location_index        += 1
-            operators[location]    = OperatorConfig(operator_name="replace",
-                                                 params={"new_value": f"Location {location_IDs[location]}"})
+        case "LOCATION":
+            location = prompt_text[result.start:result.end]
+            result.entity_type = location
+            if location not in location_IDs:
+                location_IDs[location] = location_index
+                location_index        += 1
+                operators[location]    = OperatorConfig(operator_name="replace",
+                                                        params={"new_value": f"Location {location_IDs[location]}"})
 
-    elif result.entity_type == "EMAIL_ADDRESS":
-        email = prompt_text[result.start:result.end]
-        result.entity_type = email
-        if email not in email_IDs:
-            email_IDs[email] = email_index
-            email_index     += 1
-            operators[email] = OperatorConfig(operator_name="replace",
-                                              params={"new_value": f"Email Address {email_IDs[email]}"})
+        case "EMAIL_ADDRESS":
+            email = prompt_text[result.start:result.end]
+            result.entity_type = email
+            if email not in email_IDs:
+                email_IDs[email] = email_index
+                email_index     += 1
+                operators[email] = OperatorConfig(operator_name="replace",
+                                                  params={"new_value": f"Email Address {email_IDs[email]}"})
 
-    elif result.entity_type == "IP_ADDRESS":
-        ip = prompt_text[result.start:result.end]
-        result.entity_type = ip
-        if ip not in ip_IDs:
-            ip_IDs[ip]    = ip_index
-            ip_index     += 1
-            operators[ip] = OperatorConfig(operator_name="replace",
-                                           params={"new_value": f"IP Address {ip_IDs[ip]}"})
+        case "IP_ADDRESS":
+            ip = prompt_text[result.start:result.end]
+            result.entity_type = ip
+            if ip not in ip_IDs:
+                ip_IDs[ip]    = ip_index
+                ip_index     += 1
+                operators[ip] = OperatorConfig(operator_name="replace",
+                                               params={"new_value": f"IP Address {ip_IDs[ip]}"})
 
 # Anonymize the passed prompt using the results from recognizer and custom operators
 results = anonymizer.anonymize(text=prompt_text,
